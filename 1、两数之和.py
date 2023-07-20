b9bfeceac5f7900b2c7f5a318a9fc79ca7e0ee66
lucy01 <onsite@qq.com>
2023-07-20 11:40:57 +0800

    first commit

---
+++ 1、两数之和.py
@@ -0,0 +1,15 @@
+from typing import List
+
+#List=[2,4,6,7]
+class Solution:
+    def twoSum(self,nums:List[int], target:int) -> List[int]:
+        hash = dict()
+        for i in range(len(nums)):
+            if target - nums[i] in hash:
+                return [hash[target - nums[i]], i]
+            hash[nums[i]]
+
+        return []
+
+#  两数之和提交
+
